2522a2ab3adc8b23b80e7ca572546e742804202e
whxogus215 <whxogus215@naver.com>
2023-12-03 12:08:39 +0900

    feat: 콘솔 창에 출력하는 객체 및 메시지 Enum 구현

---
+++ src/main/java/racingcar/config/OutputMessage.java
@@ -0,0 +1,16 @@
+package racingcar.config;
+
+public enum OutputMessage {
+    START("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    COUNT("시도할 회수는 몇회인가요?");
+
+    private String message;
+
+    OutputMessage(String detail) {
+        this.message = detail;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/racingcar/view/output/OutputWriter.java
@@ -0,0 +1,7 @@
+package racingcar.view.output;
+
+public class OutputWriter {
+    public static void println(Object object) {
+        System.out.println(object);
+    }
+}
