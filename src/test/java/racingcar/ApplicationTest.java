c055df152c9046bbe759e00975200a15045a94b5
whxogus215 <whxogus215@naver.com>
2023-12-06 22:23:38 +0900

    test: 입력 시 예외 발생 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.config.ErrorMessage;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,11 +16,11 @@ class ApplicationTest extends NsTest {
     @Test
     void 전진_정지() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
@@ -29,6 +30,38 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("pobi,javaji", "1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 자동차이름_중복_예외_처리() {
+        assertSimpleTest(() -> {
+            runException("aaa,aaaa,aaaa");
+            assertThat(output()).contains(ErrorMessage.NOT_DUPLICATED_CARNAME.getMessage());
+        });
+    }
+
+    @Test
+    void 자동차이름_5자_이상_예외_처리() {
+        assertSimpleTest(() -> {
+            runException("aaa,bbb,bbbbbbb");
+            assertThat(output()).contains(ErrorMessage.NOT_OVER_NAME_SIZE.getMessage());
+        });
+    }
+
+    @Test
+    void 시도횟수_문자입력_예외_처리() {
+        assertSimpleTest(() -> {
+            runException("aaa,bbb,ccc", "test");
+            assertThat(output()).contains(ErrorMessage.ONLY_NATURAL_NUMBER.getMessage());
+        });
+    }
+
+    @Test
+    void 시도횟수_1미만_예외_처리() {
+        assertSimpleTest(() -> {
+            runException("aaa,bbb,ccc", "0");
+            assertThat(output()).contains(ErrorMessage.ONLY_NATURAL_NUMBER.getMessage());
+        });
     }
 
     @Override
